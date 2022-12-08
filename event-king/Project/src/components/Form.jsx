32f9d6bff71d9def7ed895a4ca2cc7cee087c8cd
Darlene Matos <darlenem19@gmail.com>
2022-12-07 20:03:14 -0500

    Create state to manage form data input

---
+++ event-king/Project/src/components/Form.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+function Form () {
+
+    const [formData , setFormData] = useState({
+        keyword: "",
+        startDateTime: "",
+        endDateTime: "",
+        city: ""
+    })
+
+    return (
+
+        <div> 
+
+
+        </div>
+    )
+}
+
+export default Form 
