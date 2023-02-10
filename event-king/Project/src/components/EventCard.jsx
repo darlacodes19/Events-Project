07cb2735046b502286c0ef7c8657d1f22f7f700a
Darlene Matos <darlenem19@gmail.com>
2023-02-10 18:04:19 -0500

    Map over event results array and display event name

--- event-king/Project/src/components/EventCard.jsx
+++ event-king/Project/src/components/EventCard.jsx
@@ -1,8 +1,25 @@
-export function EventCard () {
+export function EventCard ({eventResults}) {
+
+    
+
+    if(!eventResults) return "null"
+    console.log(eventResults)
+
+    // const events = eventResults.map(function(element,index) {
+    //     return element
+    // })
+
+    // console.log(events)
 
     return (
         <div>
-            
+
+       {eventResults.map((e) => {
+        return (
+            <h3 key={e.id}> {e.name} </h3>
+        )
+       })}
+
         </div>
     )
 
