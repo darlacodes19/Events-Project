524b2a53e7dd9fe73b00aeb2e1f2f24ee1b52a50
Darlene Matos <darlenem19@gmail.com>
2023-02-14 14:19:16 -0500

    Use template literal instead of string concatenation due to linter rules

--- event-king/Project/src/helpers/convertTime.js
+++ event-king/Project/src/helpers/convertTime.js
@@ -1,19 +1,22 @@
-// export default function convertTime(time) {
+export default function convertTime(time) {
     
-//         let splitTime= time.split(':');
-//         let joinTime = splitTime.join();
-//         let remove = joinTime.replace(/,/g, '');
-//         let standard = parseInt(remove);
 
-//                     if (standard > 120000) {
-//                     let a= splitTime[0] -12
-//                     let standard = a + ':' + splitTime[1] + ' ' + 'PM'
-//                     return standard
-//                     } else {
-//                         return splitTime[0] + splitTime[1] + ''
+    
+        let splitTime= time.split(':');
+        let joinTime = splitTime.join();
+        let remove = joinTime.replace(/,/g, '');
+        //The parseInt function converts its first argument to a string, parses that string, then returns an integer or NaN 
+        let standard = parseInt(remove);
+
+                    if (standard > 120000) {
+                    let a= splitTime[0] -12
+                    let standard = `${a}:${splitTime[1]}PM`
+                    return standard
+                    } else {
+                        return `${splitTime[0]}${splitTime[1]}`
 
         
 
-//         }
-//     }
+        }
+    }
 
