6599a4c5146d41edc3759f207556ae46c6eed9ea
Darlene Matos <darlenem19@gmail.com>
2023-02-14 14:10:01 -0500

    Add formatDate function to change how date is display

--- event-king/Project/src/components/EventCard.jsx
+++ event-king/Project/src/components/EventCard.jsx
@@ -1,3 +1,5 @@
+import formatDate from "../helpers/formatDate"
+
 export function EventCard ({eventResults}) {
 
     
@@ -12,16 +14,14 @@ export function EventCard ({eventResults}) {
     // console.log(events)
 
     return (
-        <div>
+        <div className="events-card-div">
 
        {eventResults.map((e) => {
         return (
-            <div key={e.id}> 
+            <div key={e.id} className="event-card"> 
+            <img src={e.images[0].url}  alt="event description" className="event-image"/>
             <h3> {e.name} </h3>
-            <img src={e.images[0].url}  alt="event description"/>
-            {/* <p> {e.description}</p>
-            <p> {e.url}</p> */}
-            <p> {e.dates.start.localDate}</p>
+            <p> {formatDate(e.dates.start.localDate)}</p>
             <p> {e.dates.start.localTime}</p>
             <p> {e._embedded.venues[0].name}</p>
            <a href={e.url} target="_blank" rel="noreferrer">   <button> Get Tickets </button> </a>
